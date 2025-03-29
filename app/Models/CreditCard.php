ec82b6402eab41b0efa6514d6032930999de4b5f
Youssef Aousti <youssef@feebbo.com>
2025-03-29 14:20:39 +0100

    fix: Add HasFactory trait to models to enable factories in tests

--- app/Models/CreditCard.php
+++ app/Models/CreditCard.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CreditCard extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'bank_id',
         'product_id',
--- app/Models/ManualEdit.php
+++ app/Models/ManualEdit.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ManualEdit extends Model
 {
+    use HasFactory;
     protected $fillable = ['credit_card_id', 'field_name', 'manual_value'];
 
     public function creditCard()
